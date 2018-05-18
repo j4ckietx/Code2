358abb9ccbc92a4d6b71fda15550cd88c6b2c118
Jackie Xia <jky.x@icloud.com>
2018-05-17 22:38:35 -0700

    week9

---
+++ week9_networking_dom/sketch.js
@@ -0,0 +1,15 @@
+var gif;
+
+function myInputEvent(e) {
+  var xhr=$.get("http://api.giphy.com/v1/gifs/translate?s=" + e.target.value + "&api_key=YGXVXgGJJw8y06vezB34ihBQJGtzzG7A&limit=1");
+  xhr.done(function(response) {
+    gif.elt.src = response.data.images.fixed_height.url;
+  });
+}
+
+function setup() {
+  var inp = createInput('');
+  inp.input(myInputEvent);
+  inp.size(1000,200);
+  gif = createImg("https://media2.giphy.com/media/l0HU20BZ6LbSEITza/giphy-downsized.gif");
+}
